b5fb017d4e8a7fb8e5bc73feefc2f063c005171a
shivam <shivampatle12345@gmail.com>
2026-02-28 17:16:38 +0530

    Fix backend CORS allowing all origins

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const galleryRoutes = require('./routes/gallery');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
